4522f6e3cd00178cc3588eb76fed94acdbb8d677
Jakob <Jakob@Dragon10.hamann-software.local>
2016-01-07 12:44:35 +0100

    Added StickyNotes module!

---
+++ module/StickyNotes/src/StickyNotes/Controller/StickyNotesController.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Description of StickyNotesController
+ *
+ * @author Jakob
+ */
+namespace StickyNotes\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use StickyNotes\Model\Entity\StickyNote;
+
+class StickyNotesController extends AbstractActionController
+{
+    protected $_stickyNotesTable;
+    
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'stickynotes' => $this->getStickyNotesTable()->fetchAll(),
+        ));
+    }
+    
+    public function addAction()
+    {
+        $request = $this->getRequest();
+        $response = $this->getResponse();
+        
+        if($request->isPost())
+        {
+            $new_note = new StickyNote();
+            if (!$note_id = $this->getStickyNotesTable()->saveStickyNote($new_note))
+            {
+                $response->setContent(\Zend\Json\Json::encode(array('response' => false)));
+            }
+            else
+            {
+                $response->setContent(\Zend\Json\Json::encode(array('response' => true, 'new_note_id' => $note_id)));
+            }
+        }
+        
+        return $response;
+    }
+    
+    public function removeAction()
+    {
+        $request = $this->getRequest();
+        $response = $this->getResponse();
+        
+        if($request->isPost())
+        {
+            $post_data = $request->getPost();
+            $note_id = $post_data['id'];
+            
+            if (!$this->getStickyNotesTable()->removeStickyNote($note_id))
+            {
+                $response->setContent(\Zend\Json\Json::encode(array('response' => false)));
+            }
+            else
+            {
+                $response->setContent(\Zend\Json\Json::encode(array('response' => true)));
+            }
+            
+            return $response;
+        }
+    }
+    
+    public function updateAction()
+    {
+        $request = $this->getRequest();
+        $response = $this->getResponse();
+        
+        if($request->isPost())
+        {
+            $post_data = $request->getPost();
+            $note_id = $post_data['id'];
+            $note_content = $post_data['content'];
+            $stickynote = $this->getStickyNotesTable()->getStickyNote($note_id);
+            $stickynote->setNote($note_content);
+            
+            if (!$this->getStickyNotesTable()->saveStickyNote($stickynote))
+            {
+                $response->SetContent(\Zend\Json\Json::encode(array('response' => false)));
+            }
+            else
+            {
+                $response->SetContent(\Zend\Json\Json::encode(array('response' => true)));
+            }
+        }
+        
+        return $response;
+    }
+    
+    public function getStickyNotesTable()
+    {
+        if (!$this->_stickyNotesTable)
+        {
+            $sm = $this->getServiceLocator();
+            $this->_stickyNotesTable = $sm->get('StickyNotes\Model\StickyNotesTable');
+        }
+        return $this->_stickyNotesTable;
+    }
+}
